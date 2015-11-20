58318478eeae535d23acd547f68c0e44388810b7
Dejan Kitic <dejan.kitic@ericsson.com>
2015-11-20 15:32:08 +0000

    Added packer confguration for parallels

--- packer/scripts/install-vbox-tools.sh
+++ packer/scripts/install-vbox-tools.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-mkdir /tmp/isomount
-mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/isomount
-cd /tmp/isomount
-./VBoxLinuxAdditions.run --nox11 || true
-cd /tmp
-umount /tmp/isomount
-rm -rf /tmp/isomount
-yum remove -y gcc bzip2 kernel-ml-devel kernel-ml-headers dkms make perl
-yum -y clean all
+case "$PACKER_BUILDER_TYPE" in
+virtualbox-iso|virtualbox-ovf)
+	mkdir /tmp/isomount
+	mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/isomount
+	cd /tmp/isomount
+	./VBoxLinuxAdditions.run --nox11 || true
+	cd /tmp
+	umount /tmp/isomount
+	rm -rf /tmp/isomount
+	yum remove -y gcc bzip2 kernel-ml-devel kernel-ml-headers dkms make perl
+	yum -y clean all
+	;;
 
+parallels-iso|parallels-pvm)
+    	mkdir /tmp/parallels
+    	mount -o loop /home/vagrant/prl-tools-lin.iso /tmp/parallels
+    	/tmp/parallels/install --install-unattended-with-deps
+    	umount /tmp/parallels
+    	rmdir /tmp/parallels
+    	rm /home/vagrant/*.iso
+  	yum remove -y gcc bzip2 kernel-ml-devel kernel-ml-headers dkms make perl
+	yum -y clean all
+    	;;
+
+*)
+    	echo "Unknown Packer Builder Type >>$PACKER_BUILDER_TYPE<< selected."
+    	echo "Known are virtualbox-iso|virtualbox-ovf|parallels-iso|parallels-pvm."
+    	;;
+esac
