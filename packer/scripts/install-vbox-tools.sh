3ce5766758fbd8b6c409fc2153042f10ba50a4dc
Dejan Kitic <dejan.kitic@ericsson.com>
2016-08-10 00:41:12 +0100

    Working virtualbox provider.

--- packer/scripts/install-vbox-tools.sh
+++ packer/scripts/install-vbox-tools.sh
@@ -2,14 +2,18 @@
 
 case "$PACKER_BUILDER_TYPE" in
 virtualbox-iso|virtualbox-ovf)
-	mkdir /tmp/isomount
+        mkdir /tmp/isomount
 	mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/isomount
 	cd /tmp/isomount
 	./VBoxLinuxAdditions.run --nox11 || true
 	cd /tmp
 	umount /tmp/isomount
 	rm -rf /tmp/isomount
+        rm -rf /home/vagrant/VBoxGuestAdditions.iso
         rm -rf /opt/parallels-tools.tar.gz
+	patch /usr/src/vboxguest-5.1.2/vboxvideo/vbox_ttm.c /tmp/vboxvideo.patch
+        /sbin/rcvboxadd setup
+        rm -rf /tmp/vboxvideo.patch
 	yum remove -y gcc bzip2 kernel-ml-devel kernel-ml-headers kernel-ml-tools-libs dkms perl selinux-policy-devel
 	yum -y clean all
 	;;
